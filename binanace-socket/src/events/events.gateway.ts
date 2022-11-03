4cbe7224b7c600506716c1b7a1ab05af2470fbdd
Hoang Anh Nguyen <anh.nh@hsc.com.vn>
2022-11-03 17:37:36 +0700

    svc broadcast binance api

---
+++ binanace-socket/src/events/events.gateway.ts
@@ -0,0 +1,71 @@
+import {Logger} from '@nestjs/common';
+import {
+    MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer,
+    WsResponse,
+} from '@nestjs/websockets';
+import {from, Observable} from 'rxjs';
+import {map, tap} from 'rxjs/operators';
+import {Server, Socket} from 'socket.io';
+import * as WebSocket from "ws";
+
+@WebSocketGateway({
+    transports: ['websocket', 'polling'],
+    cors: {
+        origin: '*',
+    },
+})
+export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit {
+    @WebSocketServer()
+    server: Server;
+    private logger = new Logger('EventsGateway');
+
+    @SubscribeMessage('events')
+    findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
+        this.logger.log(data);
+        return this.getCryptoData()
+            .pipe(tap(c => {
+                this.server.emit('events', c)
+
+            }), map((c) => {
+
+                return c
+            }))
+    }
+
+
+    getCryptoData(): any {
+        const stream$ = new Observable((observer) => {
+
+            const ws: any = new WebSocket(`wss://ws.coincap.io/trades/binance`)
+            ws.on('open', () => {
+                this.logger.log('Connection established')
+            })
+            ws.onmessage = (msg: any) => {
+                const message = JSON.parse(msg.data)
+                observer.next(message)
+            }
+            ws.on('close', () => {
+                this.logger.log('Connection closed')
+            })
+        })
+        return stream$
+    }
+
+    afterInit(server: Server): any {
+        this.logger.log(server);
+    }
+
+    handleConnection(client: Socket, ...args: any[]): any {
+        this.logger.log(client.id);
+
+    }
+
+    handleDisconnect(client: Socket): any {
+        this.logger.log(client.id);
+
+    }
+
+}
