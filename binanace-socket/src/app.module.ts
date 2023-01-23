27ed7149648a91a63c4057cadfe06c68b79b6be7
Hoang Anh Nguyen <anh.nh@hsc.com.vn>
2023-01-23 16:03:38 +0700

    create endpoint

--- binanace-socket/src/app.module.ts
+++ binanace-socket/src/app.module.ts
@@ -1,9 +1,13 @@
 import {Module} from '@nestjs/common';
 import {EventsModule} from './events/events.module';
 import {ConfigModule} from "@nestjs/config";
+import {AppService} from "./app/app.service";
+import {AppController} from "./app/app.controller";
 
 @Module({
-    imports: [ConfigModule.forRoot(), EventsModule],
+    controllers: [AppController],
+    providers: [AppService],
+    // imports: [ConfigModule.forRoot(), EventsModule],
 })
 export class AppModule {
 }
