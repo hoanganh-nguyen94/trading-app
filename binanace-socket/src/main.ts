350995abbedb64c56aa0d265e3cbfc6f368f623e
Hoang Anh Nguyen <anh.nh@hsc.com.vn>
2023-01-23 15:51:57 +0700

    update vercel

--- binanace-socket/src/main.ts
+++ binanace-socket/src/main.ts
@@ -5,6 +5,13 @@ import {RedisIoAdapter} from "./adapters/redis-io.adapter";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+
+async function bootstrapWithRedis() {
+  const app = await NestFactory.create(AppModule);
+
   // Uncomment these lines to use the Redis adapter:
   const redisIoAdapter = new RedisIoAdapter(app);
   await redisIoAdapter.connectToRedis();
